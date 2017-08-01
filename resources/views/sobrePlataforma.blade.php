847f3ce8b6981025fd018df03ad80bc9cbbf1ace
mt-m2g <mt.cassinda@gmail.com>
2017-08-01 16:28:06 +0100

    Update sobrePlataforma.blade.php
    MARIANO TOMAS

--- resources/views/sobrePlataforma.blade.php
+++ resources/views/sobrePlataforma.blade.php
@@ -4,10 +4,7 @@
 
    <div class="row" style="min-height: 100%;height: 100%;margin: 0;width: 100%; padding: 0;">
 
-          <h1>Ariclinis</h1>
-
-          <h1>Ariclinis</h1>
-          <h1>Ariclinis</h1>
-        </div>
+                   <p>O Kilapy Money e uma plataforma tecnologica que visa intertigar os devedores <p>ou seja a pessoas que deseja fazer um emprestimo rapido, seguro </p> e os investidores ou seja aqueles que deseja redabilizar os seus investimentos a um juros satisfatorio.</p>
+    </div>
 
 @endsection
