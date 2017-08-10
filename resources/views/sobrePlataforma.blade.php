14fec4d1963cca8f038c3e64f82b13821e43faa9
mt-m2g <mt.cassinda@gmail.com>
2017-08-10 13:57:15 +0100

    Update sobrePlataforma.blade.php
    Outro update!

--- resources/views/sobrePlataforma.blade.php
+++ resources/views/sobrePlataforma.blade.php
@@ -3,8 +3,8 @@
 @section('conteudo')
 <br>
    <div class="row" style="min-height: 100%;height: 100%;margin: 0;width: 100%; padding: 0;"><br>
-
-        <p>O Kilapy Money e uma plataforma técnologica que visa intertigar os devedores ou seja a pessoas que deseja fazer um emprestimo rapido, seguro  e os investidores ou seja aqueles que deseja redabilizar os seus investimentos a um juros satisfatorio.</p>
+      Dar e receber empréstimos é tão normal como dar e receberm qualquer outra coisa. Eles ajudam a manter os vínculos sociais por encorajar à reciprocidade. São provavelmente antigos costumes da humanidade, que sempre confiou em mútuas permutas.<p>
+        <p>O Kilapy Money e uma plataforma técnologica que visa facilitar este processo tirando as burocraçias existentes em instituições Bancarias, a mesma oferece a pessoas que desejam fazer investimentos rápidos e seguro  e redablizar a juros satisfatorio.</p>
     </div>
 <div class="carousel-inner">
     <div class="item active">
